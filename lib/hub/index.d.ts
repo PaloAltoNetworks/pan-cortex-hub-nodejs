c335877383eaba869809ddb5a558db916d34bd94
Xavier Homs <xhoms@paloaltonetworks.com>
2020-03-19 13:12:45 +0100

    exposes DevTokenSrvResponse interface

--- lib/hub/index.d.ts
+++ lib/hub/index.d.ts
@@ -1,6 +1,6 @@
 export { Credentials } from './credentials';
 export { StaticCredentials } from './credentials_static';
-export { DevTokenCredentialsOptions, DevTokenCredentials } from './credentials_devtoken';
+export { DevTokenCredentialsOptions, DevTokenCredentials, DevTokenSrvResponse } from './credentials_devtoken';
 export { CortexCredentialProvider, CredentialProviderOptions, CredentialsItem, StoreItem, cortexCredentialsProviderFactory, SecretsStorage } from './credentials_provider';
 export { SimpleCredentialsProvider } from './credentials_provider_simple';
 export { FsCredProvider, FsCredProviderOptions } from './credentials_provider_fs';
